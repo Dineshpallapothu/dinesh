85860886aceb87623a44abbaad8190c9f4ffe477
Admin <pallapothudinesh@gmail.com>
2023-03-03 20:45:04 +0530

    strong

---
+++ contextandconfig/src/main/java/contextandconfig/cc.java
@@ -0,0 +1,39 @@
+package contextandconfig;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletConfig;
+import javax.servlet.ServletContext;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+@WebServlet("/cc")
+public class cc extends HttpServlet {
+	
+	public void serivce(HttpServletRequest req,HttpServletResponse res) throws IOException {
+		{
+			PrintWriter out=res.getWriter();
+			int x=Integer.parseInt(req.getParameter("num1"));
+			int y=Integer.parseInt(req.getParameter("num2"));
+			int k=x+y;
+			out.println(k);
+			//session
+//			
+//			ServletConfig conf=getServletConfig();
+//			String str1=conf.getInitParameter("hi");
+//			out.println("config name "+str1);
+//			
+//			ServletContext con=getServletContext();
+//			String str=con.getInitParameter("dname");
+//			out.println("context name "+str);
+			
+			
+			
+
+	}
+		
+	}
+
+}
---
+++ contextandconfig/src/main/webapp/form.jsp
@@ -0,0 +1,16 @@
+<%@ page language="java" contentType="text/html; charset=ISO-8859-1"
+    pageEncoding="ISO-8859-1"%>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="ISO-8859-1">
+<title>Insert title here</title>
+</head>
+<body>
+<form action="cc" method="post">
+enter your 1st number:<input type="text" num="num1"><br>
+enter your 1st number:<input type="text" num="num2"><br>
+<button>login</button>
+</form>
+</body>
+</html>
