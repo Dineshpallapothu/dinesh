fa79e4805bbbe834d0a1875fc127018f155fa677
ss-arunkumar <sivagunisetti@gmail.com>
2023-08-07 14:02:51 +0530

    new task

--- session/src/main/java/session/frnd.java
+++ session/src/main/java/session/frnd.java
@@ -9,6 +9,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 @WebServlet("/Calling")
+//edokka change
 public class frnd extends HttpServlet{
 	public void service(HttpServletRequest req,HttpServletResponse res) throws IOException  {
 		HttpSession s2 = req.getSession();
